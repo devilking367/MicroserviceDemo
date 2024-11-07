60848958c4fd2e729ef89e7fac654e6b1f0e6a76
devilking367 <109718002+devilking367@users.noreply.github.com>
2024-11-07 21:07:36 +0700

    init microservice

---
+++ doc-service/doc-service/src/main/java/com/thientt/doc_service/DocServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thientt.doc_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocServiceApplication.class, args);
+	}
+
+}
---
+++ doc-service/doc-service/src/test/java/com/thientt/doc_service/DocServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thientt.doc_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
